50683aecd911965f51f97406dcc39e7fac49469c
David <davidhenriquesi@live.com>
2017-08-02 17:44:09 -0300

    mann

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,24 @@
 const JwtStategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../model/user');
 const config = require('../config/database');
 
 module.exports = function(passport) {
     let opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
-    opts.secret0key = config.secret;
-    passport.use(new JwtStategy(opts, (jwt_payload, done => {
+    opts.secretOrKey = config.secret;
+    passport.use(new JwtStategy(opts, (jwt_payload, done) => {
+        User.getUserById(jwt_payload._id, (err, user) => {
 
-    })));
+            if (err) {
+                return done(err, false);
+            }
+
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    }));
 }
