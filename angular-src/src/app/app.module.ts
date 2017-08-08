76574cb1dd48369c9b89139e5cfa3c46786c9902
David <davidhenriquesi@live.com>
2017-08-08 16:56:14 -0300

    quase lá

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import{RouterModule,Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,6 +12,21 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
 
+import{ ValidateService} from './services/validate.service';
+import { FlashMessagesModule} from 'angular2-flash-messages';
+
+
+const appRoutes: Routes = 
+[
+
+{path:'', component: HomeComponent},
+{path:'register', component: RegisterComponent},
+{path:'login', component: LoginComponent},
+{path:'dashboard', component: DashboardComponent},
+{path:'profile', component: ProfileComponent}
+
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,16 +35,16 @@ import { ProfileComponent } from './profile/profile.component';
     RegisterComponent,
     HomeComponent,
     DashboardComponent,
-    DashboardComponent,
-    HomeComponent,
     ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
