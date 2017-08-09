85701b9a25a5e409e192fad08233770d1ebd770e
David <davidhenriquesi@live.com>
2017-08-09 10:51:41 -0300

    Backend fixed

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
+authtoken: any;
+user:any;
 
-  constructor() { }
+  constructor(private http:Http) { }
+
+
+registerUser(user){
+  let headers =  new Headers();
+
+  headers.append('Content-Type','application/json');
+  return this.http.post('http://localhost:300/users/register',user, {headers}).map(res =>res.json());
+}
 
 }
